5ad27c186d0ff0d1405adff6ac85560b68547d46
xian-qiqi <2893440324@qq.com>
2026-05-23 18:09:44 +0800

    移除 base 路径配置，适配 Netlify 部署

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()], // 只写一次
   // 注释掉或删除 base 配置（Netlify 不需要）
+  //github静态网页，无法使用后端接口，使用Netlify
   // base: "/AI-Mental-Health-Assistant/", // GitHub Pages部署路径
   
   server: {
